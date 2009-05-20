4b256fdf020cd79ffd535c6928137a073ec456a8
Sven Kirmess <sven.kirmess@kzone.ch>
2009-05-20 21:38:39 +0000

    SoulPouch-1
    git-svn-id: https://soul-pouch.googlecode.com/svn/tags/SoulPouch-1@2 2f805c28-4579-11de-b3c2-97ef4ed79301

---
+++ SoulPouch/localization.lua
@@ -0,0 +1,26 @@
+
+SoulPouch_localization = {}
+
+SoulPouch_localization.enUS = {
+	SOULPOUCH				= 'Soul Pouch';
+	FELCLOTHBAG				= 'Felcloth Bag';
+	COREFELCLOTHBAG				= 'Core Felcloth Bag';
+
+	WARLOCK					= 'Warlock';
+}
+
+SoulPouch_localization.deDE = {
+	SOULPOUCH				= 'Seelenbeutel';
+	FELCLOTHBAG				= 'Teufelsstofftasche';
+	COREFELCLOTHBAG				= 'Core Felcloth Bag';
+
+	WARLOCK					= 'Hexenmeister';
+}
+
+SoulPouch_localization.frFR = {
+}
+
+SoulPouch_localization.koKR = {
+}
+
+SoulPouch_Constants = SoulPouch_localization[GetLocale()]

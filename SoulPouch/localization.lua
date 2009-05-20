f947274e81a8faae02c385689c92385d066c6d73
Sven Kirmess <sven.kirmess@kzone.ch>
2009-05-20 21:48:51 +0000

    SoulPouch-7
    git-svn-id: https://soul-pouch.googlecode.com/svn/tags/SoulPouch-7@9 2f805c28-4579-11de-b3c2-97ef4ed79301

---
+++ SoulPouch/localization.lua
@@ -0,0 +1,47 @@
+
+SoulPouch_localization = {}
+
+SoulPouch_localization.enUS = {
+	SMALLSOULPOUCH				= 'Small Soul Pouch',
+	BOXOFSOULS				= 'Box of Souls',
+	SOULPOUCH				= 'Soul Pouch',
+	FELCLOTHBAG				= 'Felcloth Bag',
+	COREFELCLOTHBAG				= 'Core Felcloth Bag',
+	EBONSHADOWBAG				= 'Ebon Shadowbag',
+
+	WARLOCK					= 'Warlock',
+
+	SOUL_POUCH_LOADED			= "Soul Pouch %s loaded.",
+}
+
+SoulPouch_localization.deDE = {
+	SMALLSOULPOUCH				= 'Kleiner Seelenbeutel',
+	BOXOFSOULS				= 'Seelenkasten',
+	SOULPOUCH				= 'Seelenbeutel',
+	FELCLOTHBAG				= 'Teufelsstofftasche',
+	COREFELCLOTHBAG				= 'Kernteufelsstofftasche',
+	EBONSHADOWBAG				= 'Schwarzschattentasche',
+
+	WARLOCK					= 'Hexenmeister',
+
+	SOUL_POUCH_LOADED			= "Soul Pouch %s geladen.",
+}
+
+SoulPouch_localization.frFR = {
+	SMALLSOULPOUCH				= 'Petite bourse d\'âme',
+	BOXOFSOULS				= 'Boîte d\'âmes',
+	SOULPOUCH				= 'Bourse d\'âme',
+	FELCLOTHBAG				= 'Sac en gangrétoffe',
+	COREFELCLOTHBAG				= 'Sac en gangrétoffe du Magma',
+	EBONSHADOWBAG				= 'Ombresac d\'ébène',
+
+	WARLOCK					= 'Démoniste',
+
+	SOUL_POUCH_LOADED			= "Soul Pouch %s chargé.",
+}
+
+SoulPouch_localization.koKR = {
+}
+
+SoulPouch_Constants = SoulPouch_localization[GetLocale()]
+

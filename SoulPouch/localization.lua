5a1bb05905688c1b9509021f6d6757b6b7288433
Sven Kirmess <sven.kirmess@kzone.ch>
2009-09-09 18:42:35 +0000

    stupid...
    git-svn-id: https://soul-pouch.googlecode.com/svn/trunk@35 2f805c28-4579-11de-b3c2-97ef4ed79301

--- SoulPouch/localization.lua
+++ SoulPouch/localization.lua
@@ -3,22 +3,22 @@
 
 local SoulPouch_localization = { }
 
-local SoulPouch_localization.enUS = {
+SoulPouch_localization.enUS = {
 	SOULBAG					= 'Soul Bag',
 	SOUL_POUCH_LOADED			= "Soul Pouch %s loaded.",
 }
 
-local SoulPouch_localization.deDE = {
+SoulPouch_localization.deDE = {
 	SOULBAG					= 'Soul Bag',
 	SOUL_POUCH_LOADED			= "Soul Pouch %s geladen.",
 }
 
-local SoulPouch_localization.frFR = {
+SoulPouch_localization.frFR = {
 	SOULBAG					= 'Soul Bag',
 	SOUL_POUCH_LOADED			= "Soul Pouch %s chargȨ.",
 }
 
-local SoulPouch_localization.koKR = {
+SoulPouch_localization.koKR = {
 }
 
 SoulPouch_Constants = SoulPouch_localization[GetLocale()]
